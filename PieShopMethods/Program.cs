4dc2c81cec624790fad5cb5d61325e2563cf3f71
muren <muren@LEXICON38>
2024-03-07 16:57:07 +0100

    Add project files.

---
+++ PieShopMethods/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace PieShopMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int amount = 1234;
+            int months = 12;
+
+            int yearlyWage = Utilities.CalculateYearlyWage(amount, months);
+             Console.WriteLine($"YearlyWage : {yearlyWage}");
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ PieShopMethods/Utilities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PieShopMethods
+{
+    internal class Utilities
+    {
+
+        public static int CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked)
+        {
+            if (numberOfMonthsWorked == 12)
+                return monthlyWage * (numberOfMonthsWorked + 1);
+            return monthlyWage * numberOfMonthsWorked;
+        }
+    }
+}
